7477a8d2da2786431e2f6a6c62a78e2113a13edd
hiimtaylorjones <taylorjonessoftware@gmail.com>
2017-01-07 12:27:55 -0500

    [Setup] - imported Sean's example

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,48 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+#![feature(plugin, custom_derive)]
+#![plugin(rocket_codegen)]
+
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate diesel_codegen;
+#[macro_use] extern crate serde_derive;
+extern crate dotenv;
+extern crate rocket;
+
+pub mod schema;
+
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use std::env;
+
+use self::schema::posts;
+
+#[derive(Debug, PartialEq, Eq, Serialize, Queryable)]
+pub struct Post {
+    id: i64,
+    title: String,
+    body: String,
+}
+
+#[derive(Debug, Insertable, FromForm, AsChangeset)]
+#[table_name="posts"]
+pub struct PostForm {
+    title: String,
+    body: String,
+}
+
+impl Post {
+    pub fn new(id: i64, title: &str, body: &str) -> Self {
+        Post {
+            id: id,
+            title: title.into(),
+            body: body.into(),
+        }
     }
+}
+
+pub fn connection() -> PgConnection {
+    let _ = dotenv::dotenv();
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set or be present in a .env file");
+    PgConnection::establish(&database_url)
+        .expect("Failed to establish database connection")
 }
---
+++ src/main.rs
@@ -0,0 +1,83 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate diesel_powered_rocket;
+extern crate rocket;
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
+use diesel::prelude::*;
+use diesel_powered_rocket::*;
+use diesel_powered_rocket::schema::posts;
+use rocket_contrib::Template;
+use rocket::response::Redirect;
+use rocket::request::Form;
+
+#[get("/")]
+fn index() -> Template {
+    let connection = connection();
+    let posts = posts::table.load::<Post>(&connection)
+        .expect("Failed to load posts");
+    let context = map!["posts" => posts];
+    Template::render("index", &context)
+}
+
+#[get("/posts/new")]
+fn new_post() -> Template {
+    let context = map!["title" => "New Post"];
+    Template::render("new_post", &context)
+}
+
+#[post("/posts", data = "<post>")]
+fn create_post(post: Form<PostForm>) -> Redirect {
+    let connection = connection();
+    diesel::insert(post.get())
+        .into(posts::table)
+        .execute(&connection)
+        .expect("Failed to create post");
+    Redirect::found("/")
+}
+
+#[delete("/posts/<id>")]
+fn delete_post(id: i64) -> Redirect {
+    let connection = connection();
+    diesel::delete(posts::table.find(id))
+        .execute(&connection)
+        .expect("Failed to delete post");
+    Redirect::found("/")
+}
+
+#[get("/posts/<id>/edit")]
+fn edit_post(id: i64) -> Option<Template> {
+    #[derive(Serialize)]
+    struct Context {
+        title: &'static str,
+        post: Post,
+    }
+
+    let connection = connection();
+    let post = posts::table.find(id)
+        .first::<Post>(&connection)
+        .optional()
+        .expect("Failed to load post");
+    post.map(|post| {
+        let context = Context { title:  "Editing Post", post: post };
+        Template::render("edit_post", &context)
+    })
+}
+
+#[patch("/posts/<id>", data = "<post_data>")]
+fn update_post(id: i64, post_data: Form<PostForm>) -> Redirect {
+    let connection = connection();
+    let updated_rows = diesel::update(posts::table.find(id))
+        .set(post_data.get())
+        .execute(&connection)
+        .expect("Failed to update post");
+    assert_eq!(1, updated_rows, "Could not find a post to update");
+    Redirect::found("/")
+}
+
+fn main() {
+    rocket::ignite().mount("/", routes![index, new_post, create_post, delete_post, edit_post, update_post]).launch();
+}
